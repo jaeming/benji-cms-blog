40f52a7c2041a5c86f396ac4869a5d1f99bc58bf
Benji Dalton <daylightsavings@gmail.com>
2015-09-25 21:16:57 +1200

    converts index to haml

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+portfolio = PostType.find_or_create_by(slug: "portfolio")
+pages = PostType.find_or_create_by(slug: "page")
+posts = PostType.find_or_create_by(slug: "post")
